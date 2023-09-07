c542256b12ec49ebc9fb7e2d2c7bc14840743562
Vitaly Klyukin <delphython@gmail.com>
2023-09-07 17:15:05 +0300

    add brain-prime.js to games

---
+++ src/games/brain-prime.js
@@ -0,0 +1,27 @@
+import { app, questionsCount } from '../index.js';
+import { getRandomNumbers } from '../helpers.js';
+
+const GAME_RULE_MESSAGE = 'Answer "yes" if the number is even, otherwise answer "no".';
+
+const randomNumbers = getRandomNumbers(questionsCount);
+
+const isPrime = (number) => {
+  for (let i = 2; i < number; i++) {
+    if (number % i === 0) return false;
+  }
+  return number !== 1;
+};
+
+const getCorrectAnswers = (numbersArray) => {
+  const correctAnswersArray = [];
+
+  numbersArray.forEach(
+    (number) => correctAnswersArray.push(isPrime(number) ? 'yes' : 'no'),
+  );
+
+  return correctAnswersArray;
+};
+
+const correctAnswers = getCorrectAnswers(randomNumbers);
+
+export default () => app(GAME_RULE_MESSAGE, randomNumbers, correctAnswers);
