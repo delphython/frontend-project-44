68eb9a2b664423f60b1603c29417d9a598ae36e9
Vitaly Klyukin <delphython@gmail.com>
2023-09-18 07:48:19 +0300

    remove unnecessary parentheses

--- src/games/brain-even.js
+++ src/games/brain-even.js
@@ -4,7 +4,7 @@ import { getRandomNumbers } from '../helpers.js';
 const description = 'Answer "yes" if the number is even, otherwise answer "no".';
 const randomNumbersCount = 1;
 
-const isEven = (number) => (number % 2 === 0);
+const isEven = (number) => number % 2 === 0;
 
 const getQuestionAndAnswer = () => {
   const [question] = getRandomNumbers(randomNumbersCount);
