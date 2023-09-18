c24eefea3aaee865089032151be4ceca980d8734
Vitaly Klyukin <delphython@gmail.com>
2023-09-18 08:04:21 +0300

    refactor game

--- src/games/brain-progression.js
+++ src/games/brain-progression.js
@@ -2,14 +2,9 @@ import app from '../index.js';
 import { getRandomInt } from '../helpers.js';
 
 const description = 'What number is missing in the progression?';
-const minProgressionLength = 5;
-const maxProgressionLength = 10;
-const minProgressionStep = 2;
-const maxProgressionStep = 9;
-const minProgressionFirstNumber = 1;
-const maxProgressionFirstNumber = 20;
+const progressionLength = 10;
 
-const getProgressionArray = (firstProgressionNumber, progressionLength, progressionStep) => {
+const getProgressionArray = (firstProgressionNumber, progressionStep) => {
   const progressionArray = [];
   let count = 0;
   let nextProgressionNumber = firstProgressionNumber;
@@ -23,34 +18,20 @@ const getProgressionArray = (firstProgressionNumber, progressionLength, progress
   return progressionArray;
 };
 
-const getProgressionByString = (progressionArray, hiddenProgressionElementId) => {
-  const pArray = progressionArray;
-  const hiddenProgressionElement = pArray[hiddenProgressionElementId];
-
-  pArray[hiddenProgressionElementId] = '..';
-
-  const progressionByString = pArray.join(' ');
-
-  return [hiddenProgressionElement, progressionByString];
-};
-
 const getQuestionAndAnswer = () => {
-  const progressionLength = getRandomInt(minProgressionLength, maxProgressionLength);
-  const progressionStep = getRandomInt(minProgressionStep, maxProgressionStep);
-  const firstProgressionNumber = getRandomInt(minProgressionFirstNumber, maxProgressionFirstNumber);
+  const progressionStep = getRandomInt(2, 8);
+  const firstProgressionNumber = getRandomInt(0, 50);
 
-  const hiddenProgressionElementId = Math.floor(Math.random() * (progressionLength - 1));
+  const hiddenProgressionElementId = getRandomInt(0, 9);
 
   const progressionArray = getProgressionArray(
     firstProgressionNumber,
-    progressionLength,
     progressionStep,
   );
 
-  const [answer, question] = getProgressionByString(
-    progressionArray,
-    hiddenProgressionElementId,
-  );
+  const answer = progressionArray[hiddenProgressionElementId];
+  progressionArray[hiddenProgressionElementId] = '..';
+  const question = progressionArray.join(' ');
 
   return [question, answer.toString()];
 };
